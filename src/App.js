569744232737c4cb7891d9bfd693e5f0b57caad5
Franchise <tylerreedsullivan@gmail.com>
2020-06-30 16:33:45 -0500

    initial commit to remote

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: "Kevin De Bruyne",
+          id: "asdf3f"
+        },
+        {
+          name: "Sergio Aguero",
+          id: "asdf4"
+        },
+        {
+          name: "Raheem Sterling",
+          id: "a34ffs"
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.players.map(player => <h1 key={player.id}> {player.name} </h1>)
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
